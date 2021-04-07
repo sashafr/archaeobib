efcc69eae91e79f1acb605c8e45e5c59ded4c785
Sasha Renninger <sashafr@upenn.edu>
2021-04-07 20:10:06 +0000

    hiding availability to general users

--- items/show.php
+++ items/show.php
@@ -55,21 +55,24 @@
                             </tr>
                             <?php foreach (all_element_texts($item, array('return_type' => 'array')) as $elementset => $elements): ?>
                                 <?php foreach ($elements as $element => $elementtexts): ?>
-                                    <tr>
-                                        <th scope="row"><?php if (array_key_exists($element, $elementRenames)): echo $elementRenames[$element]; else: echo $element; endif; ?>:</th>
-                                        <td>
-                                            <?php foreach ($elementtexts as $elementtext): ?>
-                                                <?php if ($element == "External Link"): ?>
-                                                    <?php echo '<a href="' . $elementtext . '">' . $elementtext . '</a>' ?>
-                                                <?php elseif (in_array($element, $linkableElements)): ?>
-                                                    <?php $elementLookup = get_record('Element', array('name' => $element)); ?>
-                                                    <?php echo '<a href="' . url('items/browse?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D='. $elementLookup->id  . '&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=' . $elementtext) . '">' . $elementtext . '</a>'; ?></br>
-                                                <?php else: ?>
-                                                    <?php echo $elementtext; ?></br>
-                                                <?php endif; ?>
-                                            <?php endforeach ?>
-                                        </td>
-                                    </tr>
+                                    <!-- Only show availability if user is logged in -->
+                                    <?php if ($user = current_user() || $element != "Availability"): ?>
+                                        <tr>
+                                            <th scope="row"><?php if (array_key_exists($element, $elementRenames)): echo $elementRenames[$element]; else: echo $element; endif; ?>:</th>
+                                            <td>
+                                                <?php foreach ($elementtexts as $elementtext): ?>
+                                                    <?php if ($element == "External Link"): ?>
+                                                        <?php echo '<a href="' . $elementtext . '">' . $elementtext . '</a>' ?>
+                                                    <?php elseif (in_array($element, $linkableElements)): ?>
+                                                        <?php $elementLookup = get_record('Element', array('name' => $element)); ?>
+                                                        <?php echo '<a href="' . url('items/browse?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D='. $elementLookup->id  . '&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=' . $elementtext) . '">' . $elementtext . '</a>'; ?></br>
+                                                    <?php else: ?>
+                                                        <?php echo $elementtext; ?></br>
+                                                    <?php endif; ?>
+                                                <?php endforeach ?>
+                                            </td>
+                                        </tr>
+                                    <?php endif; ?>
                                 <?php endforeach; ?>
                             <?php endforeach; ?>
                         </tbody>
